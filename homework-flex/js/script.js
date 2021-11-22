f7321b7eeb5f783fa120dfe40cce4ca4bf807438
Roman Demchuk <4mmmcd@gmail.com>
2021-11-22 18:30:56 +0200

    HW FLEX

---
+++ homework-flex/js/script.js
@@ -0,0 +1,10 @@
+new Swiper('.testimonials__conteiner', {
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+   },
+   pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+   },
+});
