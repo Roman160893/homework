7865fda277a9fdca779a9206b8cfb570f9e47c17
Roman Demchuk <4mmmcd@gmail.com>
2021-12-07 17:45:28 +0200

    HW Freimwork

---
+++ homework - freimwork/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+   $(".burger").click(function (event) {
+      $(".burger,.burger__menu").toggleClass("active")
+   })
+})
+$(document).ready(function () {
+   $(".bell").click(function (event) {
+      $(".bell__menu,.bell").toggleClass("active")
+   })
+})
