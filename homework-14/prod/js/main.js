c8c362e1fa3d9bddd8e2e38d778c613b79ba279b
Roman Demchuk <4mmmcd@gmail.com>
2021-12-17 20:56:31 +0200

    fixed

---
+++ homework-14/prod/js/main.js
@@ -0,0 +1,18 @@
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+function onYouTubeIframeAPIReady() {
+   player = new YT.Player('video', {
+      height: '390',
+      width: '640',
+      videoId: 'M7lc1UVf-VE',
+   });
+}
+function onPlayerStateChange(event) {
+   if (event.data == YT.PlayerState.PLAYING && !done) {
+   }
+}
